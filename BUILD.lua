162dd211343d17fdbd207e67a4d1976760051719
Jason White <jasonaw0@gmail.com>
2016-02-26 23:53:43 -0800

    Split up BUILD.lua

--- BUILD.lua
+++ BUILD.lua
@@ -8,17 +8,7 @@ Generates the build description.
 
 local cc = require "rules.cc"
 
-cc.library {
-    name = "lua:static",
-    static = true,
-    srcs = glob {
-        "contrib/lua-5.3.2/src/*.c",
-        "!contrib/lua-5.3.2/src/lua.c",
-        "!contrib/lua-5.3.2/src/luac.c",
-        },
-    compiler_opts = {"-std=gnu99", "-O2", "-Wall", "-Wextra", "-DLUA_COMPAT_5_2"},
-    defines = {"LUA_USE_POSIX"},
-}
+import "contrib/BUILD.lua"
 
 cc.binary {
     name = "bblua",
