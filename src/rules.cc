60cba4b541650b3b7015fd7b443f93f035a48985
Jason White <jasonaw0@gmail.com>
2016-05-25 23:27:47 -0700

    Refactor rule output

--- src/rules.cc
+++ src/rules.cc
@@ -16,6 +16,12 @@ namespace {
 const char  escape_chars[] = {'\"',   '\t',  '\r',  '\n',  '\b',  '\\',  '\0'};
 const char* replacements[] = {"\\\"", "\\t", "\\r", "\\n", "\\b", "\\\\", NULL};
 
+int json_print(lua_State* L, FILE* f);
+const char* json_escape_sequence(char c);
+void json_print_string(const char* s, size_t len, FILE* f);
+int json_print_table(lua_State* L, FILE* f);
+int json_print(lua_State* L, FILE* f);
+
 /**
  * For the given character, returns the equivalent JSON escape sequence. If the
  * given character is not a character to be escaped, returns NULL.
@@ -30,36 +36,98 @@ const char* json_escape_sequence(char c) {
 }
 
 /**
- * Escapes the given string that will be output to JSON. The resulting string is
- * left at the top of the stack.
+ * Prints the given string to the given file in JSON format.
  */
-int json_escaped_string(lua_State *L, const char* s, size_t len) {
+void json_print_string(const char* s, size_t len, FILE* f) {
+    fputc('"', f); // Opening quote
 
-    size_t newlen = len;
-
-    // Calculate the new size of the escaped string
     for (size_t i = 0; i < len; ++i) {
-        if (json_escape_sequence(s[i]))
-            ++newlen;
+        if (const char* r = json_escape_sequence(s[i]))
+            fputs(r, f);
+        else
+            fputc(s[i], f);
     }
 
-    luaL_Buffer b;
-    char* buf = luaL_buffinitsize(L, &b, newlen);
-    size_t j = 0;
+    fputc('"', f); // Closing quote
+}
 
-    for (size_t i = 0; i < len; ++i) {
-        if (const char* r = json_escape_sequence(s[i])) {
-            buf[j++] = r[0];
-            buf[j++] = r[1];
-            continue;
+/**
+ * Prints the table at the top of the stack.
+ *
+ * The table is assumed to be a sequential array.
+ */
+int json_print_table(lua_State* L, FILE* f) {
+
+    fputs("[", f);
+
+    for (int i = 1; ; ++i) {
+        int type = lua_rawgeti(L, -1, i);
+
+        if (type == LUA_TNIL) {
+            lua_pop(L, 1);
+            break;
         }
 
-        buf[j++] = s[i];
+        if (i > 1)
+            fputs(", ", f);
+
+        json_print(L, f);
+
+        // Pop table element
+        lua_pop(L, 1);
     }
 
-    luaL_pushresultsize(&b, newlen);
+    fputs("]", f);
 
-    return 1;
+    return 0;
+}
+
+/**
+ * Prints the value at the top of the stack.
+ */
+int json_print(lua_State* L, FILE* f) {
+
+    switch (lua_type(L, -1))
+    {
+    case LUA_TNIL:
+        fputs("null", f);
+        break;
+
+    case LUA_TBOOLEAN:
+        fputs(lua_toboolean(L, -1) ? "true" : "false", f);
+        break;
+
+    case LUA_TNUMBER:
+        fprintf(f, "%f", lua_tonumber(L, -1));
+        break;
+
+    case LUA_TTABLE:
+        return json_print_table(L, f);
+
+    case LUA_TSTRING:
+        size_t len;
+        if (const char* s = lua_tolstring(L, -1, &len))
+            json_print_string(s, len, f);
+        break;
+
+    default:
+        return luaL_error(L, "cannot represent type %s as JSON",
+                luaL_typename(L, -1));
+    }
+
+    return 0;
+}
+
+/**
+ * Prints a single field of a JSON dictionary.
+ */
+int json_print_field(lua_State* L, const char* field, FILE* f) {
+
+    fprintf(f, "\"%s\": ", field);
+
+    json_print(L, f);
+
+    return 0;
 }
 
 }
@@ -74,123 +142,73 @@ Rules::~Rules() {
     fputs("\n]\n", _f);
 }
 
-int Rules::add(lua_State *L) {
+int Rules::add(lua_State* L) {
 
     luaL_checktype(L, 1, LUA_TTABLE);
 
     if (_n > 0)
         fputs(",", _f);
 
-    fputs("\n    {", _f);
+    fputs("\n    {\n        ", _f);
 
-    lua_getfield(L, 1, "inputs");
-    listToJSON(L, "inputs", 0);
+    // Inputs (required)
+    if (lua_getfield(L, 1, "inputs") == LUA_TTABLE)
+        json_print_field(L, "inputs", _f);
+    else
+        return luaL_error(L, "bad type for field '%s' (table expected, got %s)",
+                "inputs", luaL_typename(L, -1));
 
-    lua_getfield(L, 1, "task");
-    listToJSON(L, "task", 1);
+    // Task (required)
+    if (lua_getfield(L, 1, "task") == LUA_TTABLE) {
+        fputs(",\n        ", _f);
+        json_print_field(L, "task", _f);
+    }
+    else
+        return luaL_error(L, "bad type for field '%s' (table expected, got %s)",
+                "task", luaL_typename(L, -1));
 
-    lua_getfield(L, 1, "outputs");
-    listToJSON(L, "outputs", 2);
+    // Outputs (required)
+    if (lua_getfield(L, 1, "outputs") == LUA_TTABLE) {
+        fputs(",\n        ", _f);
+        json_print_field(L, "outputs", _f);
+    }
+    else
+        return luaL_error(L, "bad type for field '%s' (table expected, got %s)",
+                "outputs", luaL_typename(L, -1));
 
-    // Optional working directory
-    if (lua_getfield(L, 1, "cwd") != LUA_TNIL)
+    // Working directory (optional)
+    switch (lua_getfield(L, 1, "cwd"))
     {
-        stringToJSON(L, "cwd", 3);
+    case LUA_TSTRING:
+        fputs(",\n        ", _f);
+        json_print_field(L, "cwd", _f);
+        break;
+    case LUA_TNIL:
+        // Not specified
+        break;
+    default:
+        return luaL_error(L, "bad type for field '%s' (string expected, got %s)",
+                "cwd", luaL_typename(L, -1));
     }
 
-    // Optional task display
-    if (lua_getfield(L, 1, "display") != LUA_TNIL)
+    // Display (optional)
+    switch (lua_getfield(L, 1, "display"))
     {
-        stringToJSON(L, "display", 4);
+    case LUA_TSTRING:
+        fputs(",\n        ", _f);
+        json_print_field(L, "display", _f);
+        break;
+    case LUA_TNIL:
+        // Not specified
+        break;
+    default:
+        return luaL_error(L, "bad type for field '%s' (string expected, got %s)",
+                "display", luaL_typename(L, -1));
     }
 
     fputs("\n    }", _f);
 
     ++_n;
-
-    return 0;
-}
-
-int Rules::stringToJSON(lua_State *L, const char* field, size_t i) {
-
-    if (i > 0)
-        fputs(",", _f);
-
-    fprintf(_f, "\n        \"%s\": ", field);
-
-    size_t len;
-    const char* s;
-
-    s = lua_tolstring(L, -1, &len);
-    if (!s)
-        return luaL_error(L, "bad type for field '%s' (string expected, got %s)",
-                field, luaL_typename(L, -1));
-
-    json_escaped_string(L, s, len);
-    s = lua_tolstring(L, -1, &len);
-
-    fputs("\"", _f);
-    fwrite(s, 1, len, _f);
-    fputs("\"", _f);
-
-    lua_pop(L, 2); // Pop escaped string and table field
-
-    return 0;
-}
-
-int Rules::listToJSON(lua_State *L, const char* field, size_t i) {
-
-    // Which element we're on.
-    size_t element = 0;
-
-    if (i > 0)
-        fputs(",", _f);
-
-    fprintf(_f, "\n        \"%s\": ", field);
-
-    size_t len;
-    const char* s;
-
-    if (lua_type(L, -1) != LUA_TTABLE)
-        return luaL_error(L, "bad type for field '%s' (table expected, got %s)",
-                field, luaL_typename(L, -1));
-
-    fputs("[", _f);
-
-    for (int i = 1; ; ++i) {
-
-        if (lua_rawgeti(L, -1, i) == LUA_TNIL) {
-            lua_pop(L, 1);
-            break;
-        }
-
-        s = lua_tolstring(L, -1, &len);
-        if (!s)
-            return luaL_error(L,
-                    "bad type in table for field '%s' (string expected, got %s)",
-                    field, luaL_typename(L, -1));
-
-        if (element > 0)
-            fputs(", ", _f);
-
-        json_escaped_string(L, s, len);
-        const char* escaped = lua_tolstring(L, -1, &len);
-
-        fputs("\"", _f);
-        fwrite(escaped, 1, len, _f);
-        fputs("\"", _f);
-
-        lua_pop(L, 1); // Pop escaped string
-
-        ++element;
-
-        lua_pop(L, 1); // Pop table element
-    }
-
-    fputs("]", _f);
-
-    lua_pop(L, 1); // Pop table field
-
     return 0;
 }
 
